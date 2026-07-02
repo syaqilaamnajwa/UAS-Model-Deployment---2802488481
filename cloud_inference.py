6c56b46eac54cc62e9334795db1941c79709a6ee
syaqilaamnajwa <syaqilaamnajwa@gmail.com>
2026-07-02 23:58:02 +0700

    Update cloud_inference.py

--- cloud_inference.py
+++ cloud_inference.py
@@ -32,38 +32,3 @@ class SageMakerCreditScoreInference:
         )
 
         return prediction, probability_df
-
-
-if __name__ == "__main__":
-    sample = {
-        "Month": "January",
-        "Age": 42.0,
-        "Occupation": "Teacher",
-        "Annual_Income": 19214.965,
-        "Monthly_Inhand_Salary": 1730.247083,
-        "Num_Bank_Accounts": 0.0,
-        "Num_Credit_Card": 4.0,
-        "Interest_Rate": 11.0,
-        "Num_of_Loan": 0.0,
-        "Type_of_Loan": "Unknown",
-        "Delay_from_due_date": 10,
-        "Num_of_Delayed_Payment": 10.0,
-        "Changed_Credit_Limit": 4.18,
-        "Num_Credit_Inquiries": 0.0,
-        "Credit_Mix": "Good",
-        "Outstanding_Debt": 498.81,
-        "Credit_Utilization_Ratio": 37.600265,
-        "Payment_of_Min_Amount": "No",
-        "Total_EMI_per_month": 0.0,
-        "Amount_invested_monthly": 217.780472,
-        "Payment_Behaviour": "Unknown",
-        "Monthly_Balance": 245.244236,
-        "Credit_History_Age_Months": 345.0
-    }
-
-    inference = SageMakerCreditScoreInference()
-    prediction, probability_df = inference.predict(sample)
-
-    print("Prediction:", prediction)
-    print("\nPrediction Probability:")
-    print(probability_df)
